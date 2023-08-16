1f2e13e2d5df8c23beaf32586d0243a79349ae23
Tehnoload <11tehnoload11@gmail.com>
2023-08-16 10:21:30 +0300

    fix

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,35 @@
+const refs = {
+  startBtn: document.querySelector('[data-start]'),
+  stopBtn: document.querySelector('[data-stop]'),
+};
 
+const colorChanges = {
+  intervalId: null,
+  isActive: false,
+  start() {
+    this.intervalId = setInterval(() => {
+      document.body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+  },
+  stop() {
+    clearInterval(this.intervalId);
+  },
+};
+
+refs.startBtn.addEventListener('click', () => {
+  refs.startBtn.disabled = true;
+  refs.stopBtn.disabled = false;
+  colorChanges.start();
+});
+
+refs.stopBtn.addEventListener('click', () => {
+  refs.startBtn.disabled = false;
+  refs.stopBtn.disabled = true;
+  colorChanges.stop();
+});
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
